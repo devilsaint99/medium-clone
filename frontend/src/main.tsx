64cc41fdc66c9ac1f7ae7f7cf692abdf6b3f01ad
Devilsaint <anshul.suresh99@gmail.com>
2024-08-24 00:13:38 +0530

    removed unused code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,26 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
-import Root from "./routes/Root";
-import ErrorPage from "./error-page.tsx"
 import App from './App.tsx'
 import './index.css'
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <Root />
-    ),
-    errorElement: <ErrorPage />
-  }
-]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
