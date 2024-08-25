1ac753e1917105d99ef01befedc1e24f816b02d7
Devilsaint <anshul.suresh99@gmail.com>
2024-08-25 20:08:06 +0530

    fixed backend api root url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,4 @@
 
-export const BACKEND_URL = "http://127.0.0.1:8787"
+// export const BACKEND_URL = "http://127.0.0.1:8787"
 //prod
-
-// export const BACKEND_URL = "https://backend.anshul-suresh99.workers.dev"
+export const BACKEND_URL = "https://backend.anshul-suresh99.workers.dev"
